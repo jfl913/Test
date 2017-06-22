218046bdd16e1076c9d27e4d3df8ab8d3974ea6f
李俊峰 <lijunfeng@u51.com>
2017-06-22 15:02:25 +0800

    delete

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -8,11 +8,9 @@
 
 #import "ViewController.h"
 #import "Test.h"
-#import "Xtrace.h"
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
@@ -21,17 +19,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    self.testLabel.text = @"潇洒";
 }
 
 - (IBAction)tapButton:(id)sender
 {
-    NSComparisonResult result1 = [@"A" compare:@"B"];
-    NSLog(@"A compare B : %ld", (long)result1);
     
-    NSComparisonResult result2 = [@"2017-03-10" compare:@"2017-04-24"];
-    NSLog(@"2017-03-10 compare 2017-04-24 : %ld", (long)result2);
 }
 
 @end
