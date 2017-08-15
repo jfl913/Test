310b1964d7027367bd4ff22bae23b8d7c475c555
李俊峰 <lijunfeng@u51.com>
2017-08-15 19:17:02 +0800

    no message

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -23,7 +23,20 @@
 
 - (IBAction)tapButton:(id)sender
 {
-    
+    NSString *testString = @"*9*^&29";
+    NSString *regexPattern = @"\\d+";
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:regexPattern
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:nil];
+    NSArray *resultArray = [regex matchesInString:testString
+                                          options:NSMatchingReportCompletion
+                                            range:NSMakeRange(0, testString.length)];
+    for (NSInteger i = resultArray.count - 1; i >= 0; i--) {
+        NSTextCheckingResult *result = resultArray[i];
+        NSRange resultRange = result.range;
+        NSString *resultString = [testString substringWithRange:resultRange];
+        NSLog(@"resultString: %@", resultString);
+    }
 }
 
 @end
