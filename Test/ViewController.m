f9f7be50daece9011a1c93e2c019b0f08af023ad
李俊峰 <lijunfeng@u51.com>
2017-11-03 17:30:53 +0800

    no message

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -23,7 +23,26 @@
 
 - (IBAction)tapButton:(id)sender
 {
-    
+    NSString *test = @"<font color=\"#999999\">2</font><font color=\"#999999\">2</font>";
+    NSString *result = [self removeHTML:test];
+    NSLog(@"%@", result);
+}
+
+- (NSString *)removeHTML:(NSString *)html {
+    NSScanner *theScanner;
+    NSString *text = nil;
+    theScanner = [NSScanner scannerWithString:html];
+    while ([theScanner isAtEnd] == NO) {
+        // find start of tag
+        [theScanner scanUpToString:@"<" intoString:NULL] ;
+        // find end of tag
+        [theScanner scanUpToString:@">" intoString:&text] ;
+        // replace the found tag with a space
+        //(you can filter multi-spaces out later if you wish)
+        html = [html stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@>", text] withString:@" "];
+    } 
+
+    return html; 
 }
 
 @end
