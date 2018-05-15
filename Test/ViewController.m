9ee21eb25eba161f37a338cb736f56638d18e649
李俊峰 <lijunfeng@u51.com>
2018-05-15 17:46:26 +0800

    no message

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -10,18 +10,34 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *htmlLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self tapButton:nil];
 }
 
-- (IBAction)tapButton:(id)sender
-{
+- (IBAction)tapButton:(id)sender {
+    NSString *jsonPath = [[NSBundle mainBundle] pathForResource:@"html" ofType:@"json"];
+    NSData *data = [NSData dataWithContentsOfFile:jsonPath];
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+    NSString *htmlStr = dict[@"html"];
+    
+    NSData *htmlData = [htmlStr dataUsingEncoding:NSUnicodeStringEncoding];
+    NSDictionary *optionDict = @{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType};
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithData:htmlData
+                                                                                          options:optionDict
+                                                                               documentAttributes:nil
+                                                                                            error:nil];
+//    [attributedString addAttribute:NSFontAttributeName
+//                             value:[UIFont systemFontOfSize:14.0]
+//                             range:NSMakeRange(0, attributedString.length)];
+    self.htmlLabel.attributedText = attributedString;
     
 }
 
