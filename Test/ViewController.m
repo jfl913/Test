35f2f2c6af325bc1d0e4f724ef00cc3d69aceb6e
junfeng Li <jfl913@163.com>
2020-05-04 23:45:59 +0800

    no message

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, assign) NSInteger count;
+@property (nonatomic, strong) NSLock *lock;
+@property (nonatomic, strong) NSRecursiveLock *recursiveLock;
 
 @end
 
@@ -17,10 +20,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.count = 5;
+    self.lock = [NSLock new];
+    self.recursiveLock = [NSRecursiveLock new];
 }
 
 - (IBAction)tapButton:(id)sender {
-    
+//    [self testSynchronized];
+//    [self testNSLock];
+    [self testNSRecursiveLock];
+}
+
+- (void)testSynchronized {
+    if (self.count > 0) {
+        @synchronized (self) {
+            NSLog(@"count = %@", @(self.count));
+            self.count = self.count - 1;
+            [self testSynchronized];
+        }
+    }
+}
+
+- (void)testNSLock {
+    [self.lock lock];
+    if (self.count > 0) {
+        NSLog(@"count = %@", @(self.count));
+        self.count = self.count - 1;
+        [self testNSLock];
+    }
+    [self.lock unlock];
+}
+
+- (void)testNSRecursiveLock {
+    [self.recursiveLock lock];
+    if (self.count > 0) {
+        NSLog(@"count = %@", @(self.count));
+        self.count = self.count - 1;
+        [self testNSRecursiveLock];
+    }
+    [self.recursiveLock unlock];
 }
 
 @end
--- Test/main.m
+++ Test/main.m
@@ -14,3 +14,11 @@ int main(int argc, char * argv[]) {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+void testSync()
+{
+    NSObject* obj = [NSObject new];
+    @synchronized (obj) {
+        
+    }
+}
