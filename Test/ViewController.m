0a8be64eebb8b650f15371e93249a532eb965477
junfeng Li <jfl913@163.com>
2020-05-06 17:25:55 +0800

    no message

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -20,7 +20,49 @@
 }
 
 - (IBAction)tapButton:(id)sender {
-    
+//    [self testMRCBlockType];
+    [self testMRCStackBlock];
 }
+
+// 需要设置成 MRC 环境。
+// https://www.jianshu.com/p/115e6d9d956c
+- (void)testMRCBlockType {
+    void (^globalBlock)(void) = ^{
+        NSLog(@"globalBlock");
+    };
+    
+    NSInteger a = 10;
+    void (^stackBlock)(void) = ^{
+        NSLog(@"stackBlock a = %@", @(a));
+    };
+    
+    NSLog(@"globalBlock type: %@, stackBlock type: %@", [globalBlock class], [stackBlock class]);
+    NSLog(@"mallocBlock type: %@", [[stackBlock copy] class]);
+}
+
+void (^stackBlock)(void);
+
+- (void)testMRCStackBlock {
+    NSLog(@"stackBlock type: %@", [stackBlock class]);
+    [self createMRCStackBlock];
+    stackBlock();
+    // 再调一次就会崩溃。
+    stackBlock();
+}
+
+
+
+- (void)createMRCStackBlock {
+    NSInteger a = 11;
+    stackBlock = ^{
+        NSLog(@"stackBlock a = %@", @(a));
+    };
+//    stackBlock = [^{
+//        NSLog(@"stackBlock a = %@", @(a));
+//    } copy];
+    NSLog(@"stackBlock reference auto var type: %@", [stackBlock class]);
+}
+
+- (void)
 
 @end
--- Test/main.m
+++ Test/main.m
@@ -9,8 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+void (^block)(void);
+void test()
+{
+    // __NSStackBlock__
+    int a = 10;
+    block = ^{
+        NSLog(@"block---------%d", a);
+    };
+}
+
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        test();
+        block();
+//        block();
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
